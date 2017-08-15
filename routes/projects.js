6c6bc5fd8bf102b69dcd8aaf75c0429140fd0731
Aleksey-Kolyasa-dev <aleksey.kolyasa.dev@gmail.com>
2017-08-15 06:10:11 +0300

    - NEW PROJECT Ctor now on SERVER Side

--- routes/projects.js
+++ routes/projects.js
@@ -41,14 +41,432 @@ projectsRouter.get('/api/:id', function (req, res, next) {
 });
 // POST Single Project
 projectsRouter.post('/api', function (req, res, next) {
-   var newProject = req.body;
-   if(!newProject.fianceName || !newProject.fianceeName || !newProject.weddingDate || !newProject.budget.budgetUSD || !newProject.email || !newProject.telephones) {
+    // New Project Constructor
+    function NewProjectCtor(project) {
+        // INIT DATA
+        this.owner = project.owner;
+        this.fianceName = project.fianceName;
+        this.fianceeName = project.fianceeName;
+        this.weddingDate = project.weddingDate;
+        this.email = project.email;
+        this.telephones = project.telephones;
+        this.notes = project.notes;
+        this.fianceSideGuests = [];
+        this.fianceeSideGuests = [];
+
+        //NOTES
+        this.budgetNotes = null;
+        this.restNotes = null;
+        this.guestsNotes = null;
+        this.menuNotes = null;
+        this.cakesNotes = null;
+        this.plusNotes = null;
+        this.decorNotes = null;
+
+        //INTERMEDIATE DATA
+        this.useMenuCheck = false;
+
+        // MENU DATA
+        this.budget = {
+            budgetUSD: project.wedBudget,
+            budgetNat: 0,
+            nationalMoney: 'units',
+            currency: 1,
+            total: {
+                // TTL SUM ALL PLANS
+                wedPlanTotalNat: 0,
+                wedPlanTotalUsd: 0,
+
+                // TTL SUM ALL PAID
+                wedPaidTotalNat: 0,
+                wedPaidTotalUsd: 0,
+
+                // TTL SUM ALL REST
+                wedRestTotalNat: 0,
+                wedRestTotalUsd: 0,
+
+                // TOTAL WED BUDGET REST by PLAN
+                wedBudgetRestPlanUsd: 0,
+                wedBudgetRestPlanNat: 0,
+
+                // TOTAL WED BUDGET REST by FACT
+                wedBudgetRestFactUsd: 0,
+                wedBudgetRestFactNat: 0
+            }
+        };
+        this.restaurant = {
+            name: null,
+            address: null,
+            telephones: null,
+            website: null,
+            guestsQty: 2,
+            quickView: false,
+            quickData: {
+                quickGuestsQty: 2,
+                quickCheck: 0,
+                quickPercent: 0,
+                quickPlugs: 0,
+                planNat: 0,
+                planUsd: 0
+            },
+            generalData: {
+                generalCheck: 0,
+                generalPercent: 0,
+                generalPlugs: 0,
+
+                sumCheckNat: 0,
+                sumPercentNat: 0,
+                sumPlugsNat: 0,
+                fullCheckNat: 0
+            },
+            total: {
+                planUsd: 0,
+                planNat: 0,
+                paidUsd: 0,
+                paidNat: 0,
+                restUsd: 0,
+                restNat: 0,
+                planTotalUsd: 0,
+                planTotalNat: 0,
+                paidTotalUsd: 0,
+                paidTotalNat: 0,
+                restTotalUsd: 0,
+                restTotalNat: 0
+            }
+        };
+        this.restaurantMenu = {
+            expCollection: [
+                /* {
+                 name : 'ТОРТ', // *<input S
+                 portionWeight : 0, // *<input N
+                 portionPrice : 0, // *<input N
+                 portionQty : 0, // *<input N
+                 toPay : 0, // portionPrice * portionQty
+                 totalWeight: 0, // portionWeight * portionQty
+                 category : null // *<input S
+                 }*/
+            ],
+            total: {
+                totalMenuPriceNat: 0, // sum[toPai]
+                totalMenuWeight: 0, // sum[totalWeight]
+
+                calculatedCheck: 0, // sum[toPay] / guestsQty
+                weightPerGuest: 0 // sum[totalWeight] / guestsQty
+            },
+            categories: [
+                "Холодные закуски",
+                "Салаты",
+                "Горячие закуски",
+                "Основные блюда",
+                "Дисерт",
+                "Напитки А",
+                "Напитки Б/А",
+                "Фуршет",
+                "Инное"
+            ]
+        };
+        this.restaurantCakes = {
+            expCollection: [
+                /*{
+                 name : 'ТОРТ', // *<input S
+                 grPerGuest : 0, // *<input N
+                 totalKg : 0, // guestsQ * grPerGuest
+                 kgPrice : 0, // *<input N
+                 toPai : 0, // totalKg * kgPrice
+                 paid : 0, // *<input N
+                 rest : 0, // toPai - paid
+                 usd : false,
+                 money : null
+                 }*/
+            ],
+            total: {
+                planUsd: 0, // planNat / currency
+                planNat: 0, // sum[toPai]
+                paidUsd: 0, // paidNat / currency
+                paidNat: 0, // sum[paid]
+                paidTotalUsd: 0, //
+                paidTotalNat: 0, //
+                restTotalUsd: 0,
+                restTotalNat: 0
+            }
+        };
+        this.restaurantPlus = {
+            expCollection: [],
+            total: {
+                planUsd: 0,
+                planNat: 0,
+                paidUsd: 0,
+                paidNat: 0,
+                paidTotalUsd: 0,
+                paidTotalNat: 0,
+                restTotalUsd: 0,
+                restTotalNat: 0
+            }
+        };
+        this.decor = {
+            expCollection: [/*{
+             name : 'ТОРТ',
+             tariff : 200,
+             multiplier : 5,
+             unit : 'kg',
+             toPai : 0,
+             paid : 0,
+             rest : 0,
+             usd : false,
+             money : null
+             }*/],
+            total: {
+                planUsd: 0,
+                planNat: 0,
+                paidUsd: 0,
+                paidNat: 0,
+                paidTotalUsd: 0,
+                paidTotalNat: 0,
+                restTotalUsd: 0,
+                restTotalNat: 0
+            }
+        };
+        this.flower = {
+            expCollection: [/*{
+             name : 'ТОРТ',
+             tariff : 200,
+             multiplier : 5,
+             unit : 'kg',
+             toPai : 0,
+             paid : 0,
+             rest : 0,
+             usd : false,
+             money : null
+             }*/],
+            total: {
+                planUsd: 0,
+                planNat: 0,
+                paidUsd: 0,
+                paidNat: 0,
+                paidTotalUsd: 0,
+                paidTotalNat: 0,
+                restTotalUsd: 0,
+                restTotalNat: 0
+            }
+        };
+        this.leader = {
+            expCollection: [/*{
+             name : 'ТОРТ',
+             tariff : 200,
+             multiplier : 5,
+             unit : 'kg',
+             toPai : 0,
+             paid : 0,
+             rest : 0,
+             usd : false,
+             money : null
+             }*/],
+            total: {
+                planUsd: 0,
+                planNat: 0,
+                paidUsd: 0,
+                paidNat: 0,
+                paidTotalUsd: 0,
+                paidTotalNat: 0,
+                restTotalUsd: 0,
+                restTotalNat: 0
+            }
+        };
+        this.music = {
+            expCollection: [/*{
+             name : 'ТОРТ',
+             tariff : 200,
+             multiplier : 5,
+             unit : 'kg',
+             toPai : 0,
+             paid : 0,
+             rest : 0,
+             usd : false,
+             money : null
+             }*/],
+            total: {
+                planUsd: 0,
+                planNat: 0,
+                paidUsd: 0,
+                paidNat: 0,
+                paidTotalUsd: 0,
+                paidTotalNat: 0,
+                restTotalUsd: 0,
+                restTotalNat: 0
+            }
+        };
+        this.photo = {
+            expCollection: [/*{
+             name : 'ТОРТ',
+             tariff : 200,
+             multiplier : 5,
+             unit : 'kg',
+             toPai : 0,
+             paid : 0,
+             rest : 0,
+             usd : false,
+             money : null
+             }*/],
+            total: {
+                planUsd: 0,
+                planNat: 0,
+                paidUsd: 0,
+                paidNat: 0,
+                paidTotalUsd: 0,
+                paidTotalNat: 0,
+                restTotalUsd: 0,
+                restTotalNat: 0
+            }
+        };
+        this.video = {
+            expCollection: [/*{
+             name : 'ТОРТ',
+             tariff : 200,
+             multiplier : 5,
+             unit : 'kg',
+             toPai : 0,
+             paid : 0,
+             rest : 0,
+             usd : false,
+             money : null
+             }*/],
+            total: {
+                planUsd: 0,
+                planNat: 0,
+                paidUsd: 0,
+                paidNat: 0,
+                paidTotalUsd: 0,
+                paidTotalNat: 0,
+                restTotalUsd: 0,
+                restTotalNat: 0
+            }
+        };
+        this.zags = {
+            expCollection: [/*{
+             name : 'ТОРТ',
+             tariff : 200,
+             multiplier : 5,
+             unit : 'kg',
+             toPai : 0,
+             paid : 0,
+             rest : 0,
+             usd : false,
+             money : null
+             }*/],
+            total: {
+                planUsd: 0,
+                planNat: 0,
+                paidUsd: 0,
+                paidNat: 0,
+                paidTotalUsd: 0,
+                paidTotalNat: 0,
+                restTotalUsd: 0,
+                restTotalNat: 0
+            }
+        };
+        this.transport = {
+            expCollection: [/*{
+             name : 'ТОРТ',
+             tariff : 200,
+             multiplier : 5,
+             unit : 'kg',
+             toPai : 0,
+             paid : 0,
+             rest : 0,
+             usd : false,
+             money : null
+             }*/],
+            total: {
+                planUsd: 0,
+                planNat: 0,
+                paidUsd: 0,
+                paidNat: 0,
+                paidTotalUsd: 0,
+                paidTotalNat: 0,
+                restTotalUsd: 0,
+                restTotalNat: 0
+            }
+        };
+        this.other = {
+            expCollection: [/*{
+             name : 'ТОРТ',
+             tariff : 200,
+             multiplier : 5,
+             unit : 'kg',
+             toPai : 0,
+             paid : 0,
+             rest : 0,
+             usd : false,
+             money : null
+             }*/],
+            total: {
+                planUsd: 0,
+                planNat: 0,
+                paidUsd: 0,
+                paidNat: 0,
+                paidTotalUsd: 0,
+                paidTotalNat: 0,
+                restTotalUsd: 0,
+                restTotalNat: 0
+            }
+        };
+        this.dressM = {
+            expCollection: [/*{
+             name : 'ТОРТ',
+             tariff : 200,
+             multiplier : 5,
+             unit : 'kg',
+             toPai : 0,
+             paid : 0,
+             rest : 0,
+             usd : false,
+             money : null
+             }*/],
+            total: {
+                planUsd: 0,
+                planNat: 0,
+                paidUsd: 0,
+                paidNat: 0,
+                paidTotalUsd: 0,
+                paidTotalNat: 0,
+                restTotalUsd: 0,
+                restTotalNat: 0
+            }
+        };
+        this.dressW = {
+            expCollection: [/*{
+             name : 'ТОРТ',
+             tariff : 200,
+             multiplier : 5,
+             unit : 'kg',
+             toPai : 0,
+             paid : 0,
+             rest : 0,
+             usd : false,
+             money : null
+             }*/],
+            total: {
+                planUsd: 0,
+                planNat: 0,
+                paidUsd: 0,
+                paidNat: 0,
+                paidTotalUsd: 0,
+                paidTotalNat: 0,
+                restTotalUsd: 0,
+                restTotalNat: 0
+            }
+        };
+    }
+
+    var newProject = req.body;
+
+   if(!newProject.owner || !newProject.fianceName || !newProject.fianceeName || !newProject.weddingDate || !newProject.wedBudget || !newProject.email || !newProject.telephones) {
        res.status(400);
        res.json({
            "error" : "POST ERROR: not all required fields was filled in"
        });
    } else {
-       db.weddings.save(newProject, function (err, newProject) {
+       db.weddings.save(new NewProjectCtor(newProject), function (err, newProject) {
            if(err){
                res.send(err);
            }
