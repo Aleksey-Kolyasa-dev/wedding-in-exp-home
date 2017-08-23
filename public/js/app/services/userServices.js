260fd6ecc154014506feba716755e4d969110007
Aleksey-Kolyasa-dev <aleksey.kolyasa.dev@gmail.com>
2017-08-23 09:17:15 +0300

    -- AFTER SUSSESS DEPLOY

--- public/js/app/services/userServices.js
+++ public/js/app/services/userServices.js
@@ -9,8 +9,8 @@ define(['angular'], function (angular) {
 
     function UsersResourceService(toastr, $http, $q, $log, $location, wedURL) {
         return {
-            //baseURL: 'http://localhost:5000/users/',
-            baseURL: 'https://wedding-in.herokuapp.com/users/',
+            baseURL: 'http://localhost:5000/users/',
+            //baseURL: 'https://wedding-in.herokuapp.com/users/',
             _ajaxRequest: function (method, url, data, keyURL) {
                 var self = this;
                 var deferred = $q.defer();
