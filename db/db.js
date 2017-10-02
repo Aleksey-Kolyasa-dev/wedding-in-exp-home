dc3ca66d16c9b424ce283bc0f778c5ea4cb1fc90
Aleksey-Kolyasa-dev <aleksey.kolyasa.dev@gmail.com>
2017-10-02 22:39:43 +0300

    --db #1

--- db/db.js
+++ db/db.js
@@ -11,16 +11,18 @@ function _envAPI() {
         _dev : apiDevEnvironment,
         get _projectsDB_URL(){
             if(apiDevEnvironment){
-                return mongojs('mongodb://localhost:27017/weddings', ['weddings']);
+                return mongojs('mongodb://localhost:27017/weddings', ['weddings']); // mongodb://185.69.154.110:27017/wedUsers
             } else {
-                return mongojs('mongodb://alex:4444@ds149132.mlab.com:49132/alkol_db', ['weddings']);
+                //return mongojs('mongodb://alex:4444@ds149132.mlab.com:49132/alkol_db', ['weddings']);
+                return mongojs('mongodb://185.69.154.110:27017/weddings', ['weddings']);
             }
         },
         get _usersDB_URL(){
             if(apiDevEnvironment){
                 return mongojs('mongodb://localhost:27017/wedUsers', ['wedUsers']);
             } else {
-                return mongojs('mongodb://alex:4444@ds149132.mlab.com:49132/alkol_db', ['wedUsers']);
+                //return mongojs('mongodb://alex:4444@ds149132.mlab.com:49132/alkol_db', ['wedUsers']);
+                return mongojs('mongodb://185.69.154.110:27017/wedUsers', ['wedUsers']);
             }
         }
     };
