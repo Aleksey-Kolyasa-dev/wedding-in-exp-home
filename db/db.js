d83302cbaaac9d94f25d1f2a64d2fb0ea31366d3
Aleksey-Kolyasa-dev <aleksey.kolyasa.dev@gmail.com>
2017-10-03 10:11:35 +0200

    --db mongolab

--- db/db.js
+++ db/db.js
@@ -4,7 +4,7 @@ var mongojs = require('mongojs');
 // ENVIRONMENT SWITCH Fn
 function _envAPI() {
     // MAIN SET
-    var apiDevEnvironment = true;
+    var apiDevEnvironment = false;
 
     // Auto
     return {
@@ -13,16 +13,16 @@ function _envAPI() {
             if(apiDevEnvironment){
                 return mongojs('mongodb://localhost:27017/weddings', ['weddings']); // mongodb://185.69.154.110:27017/wedUsers
             } else {
-                //return mongojs('mongodb://alex:4444@ds149132.mlab.com:49132/alkol_db', ['weddings']);
-                return mongojs('mongodb://185.69.154.110:27017/weddings', ['weddings']);
+                return mongojs('mongodb://alex:4444@ds149132.mlab.com:49132/alkol_db', ['weddings']);
+                //return mongojs('mongodb://185.69.154.110:27017/weddings', ['weddings']);
             }
         },
         get _usersDB_URL(){
             if(apiDevEnvironment){
                 return mongojs('mongodb://localhost:27017/wedUsers', ['wedUsers']);
             } else {
-                //return mongojs('mongodb://alex:4444@ds149132.mlab.com:49132/alkol_db', ['wedUsers']);
-                return mongojs('mongodb://185.69.154.110:27017/wedUsers', ['wedUsers']);
+                return mongojs('mongodb://alex:4444@ds149132.mlab.com:49132/alkol_db', ['wedUsers']);
+                //return mongojs('mongodb://185.69.154.110:27017/wedUsers', ['wedUsers']);
             }
         }
     };
