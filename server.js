98c50d1e3db804d9dc9364a7ca347005fe9e90a4
Aleksey-Kolyasa-dev <aleksey.kolyasa.dev@gmail.com>
2017-10-03 11:41:46 +0200

    Access-Control

--- server.js
+++ server.js
@@ -19,6 +19,12 @@ app.use('/api', require('./routes/projects'));
 app.use('/users', require('./routes/users'));
 // Static folder
 app.use(express.static(path.join(__dirname, './public')));
+app.use(function (req,res, next) {
+   res.header('Access-Control-Allow-Origin', "*");
+   res.header('Access-Control-Allow-Methods', "GET,PUT,POST, DELETE");
+   res.header('Access-Control-Allow-Headers', "Content-Type");
+   next();
+});
 
 // Cross Origin Access Control
 app.use(cors());
