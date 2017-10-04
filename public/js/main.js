f11930c53c121314c8cf016c9b5098372d570bd6
Aleksey-Kolyasa-dev <aleksey.kolyasa.dev@gmail.com>
2017-10-04 11:29:33 +0200

    -- ORIGIN TIME WEB

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,12 @@
 // ENVIRONMENT SWITCH Fn
 function _env() {
-	// MAIN SET
-	var devEnvironment = false;
+    // MAIN SET
+    var devEnvironment = false;
 
-	// Auto
+    // Auto
     return {
         _dev : devEnvironment,
-		get _apiURL(){
+        get _apiURL(){
             if(devEnvironment){
                 return 'http://localhost:80/api/';
             } else {
@@ -14,8 +14,8 @@ function _env() {
                 //return 'http://wedding-in.com.ua/api/';
                 return 'http://176.57.214.217:80/api/';
             }
-		},
-		get _usersURL(){
+        },
+        get _usersURL(){
             if(devEnvironment){
                 return 'http://localhost:80/users/';
             } else {
@@ -23,7 +23,7 @@ function _env() {
                 //return 'http://wedding-in.com.ua/users/';
                 return 'http://176.57.214.217:80/users/';
             }
-		},
+        },
         get _angularRuLocale(){
             if(devEnvironment){
                 return '../libs/js/misc/jquery.ui.datepicker-ru';
@@ -36,138 +36,138 @@ function _env() {
 
 // Require.js main settings
 require.config({
-	baseUrl : '',
-	paths : 
-	{
-		'app'             		: '../js/app/app',
-		'projServices'        		: '../js/app/services/projectServices',
-		'authServices'          : '../js/app/services/userServices',
-		'controllers'     		: '../js/app/controllers/01_wedMainCtrl',
-        'usersCtrlModule'    	: '../js/app/controllers/01_users/01_usersMainCtrl',
-        'adminCtrlModule'    	: '../js/app/controllers/01_users/02_adminMainCtrl',
-		'newProjectCtrlModule' 	: '../js/app/controllers/02_projects/02_wedMain_newProjectCtrl',
-        'budgetCtrlModule' 		: '../js/app/controllers/02_projects/03_BudgetCtrl',
-        'restaurantCtrlModule' 	: '../js/app/controllers/02_projects/04_RestaurantCtrl',
-        'arrangementCtrlModule' : '../js/app/controllers/02_projects/05_ArrangementCtrl',
-        'programCtrlModule'     : '../js/app/controllers/02_projects/06_ProgramCtrl',
-        'filmingCtrlModule'     : '../js/app/controllers/02_projects/07_FilmingCtrl',
+    baseUrl : '',
+    paths :
+        {
+            'app'             		: '../js/app/app',
+            'projServices'        		: '../js/app/services/projectServices',
+            'authServices'          : '../js/app/services/userServices',
+            'controllers'     		: '../js/app/controllers/01_wedMainCtrl',
+            'usersCtrlModule'    	: '../js/app/controllers/01_users/01_usersMainCtrl',
+            'adminCtrlModule'    	: '../js/app/controllers/01_users/02_adminMainCtrl',
+            'newProjectCtrlModule' 	: '../js/app/controllers/02_projects/02_wedMain_newProjectCtrl',
+            'budgetCtrlModule' 		: '../js/app/controllers/02_projects/03_BudgetCtrl',
+            'restaurantCtrlModule' 	: '../js/app/controllers/02_projects/04_RestaurantCtrl',
+            'arrangementCtrlModule' : '../js/app/controllers/02_projects/05_ArrangementCtrl',
+            'programCtrlModule'     : '../js/app/controllers/02_projects/06_ProgramCtrl',
+            'filmingCtrlModule'     : '../js/app/controllers/02_projects/07_FilmingCtrl',
 
-		'registrationCtrlModule'     : '../js/app/controllers/02_projects/08_RegistrationCtrl',
-        'marriageCtrlModule'     : '../js/app/controllers/02_projects/09_MarriageCtrl',
-        'transportCtrlModule'     : '../js/app/controllers/02_projects/10_TransportCtrl',
+            'registrationCtrlModule'     : '../js/app/controllers/02_projects/08_RegistrationCtrl',
+            'marriageCtrlModule'     : '../js/app/controllers/02_projects/09_MarriageCtrl',
+            'transportCtrlModule'     : '../js/app/controllers/02_projects/10_TransportCtrl',
 
 
-        'dressCtrlModule' 		: '../js/app/controllers/02_projects/11_DressCtrl',
-        'partyCtrlModule' 		: '../js/app/controllers/02_projects/12_PartyCtrl',
+            'dressCtrlModule' 		: '../js/app/controllers/02_projects/11_DressCtrl',
+            'partyCtrlModule' 		: '../js/app/controllers/02_projects/12_PartyCtrl',
 
-        'otherCtrlModule'     : '../js/app/controllers/02_projects/13_OtherCtrl',
+            'otherCtrlModule'     : '../js/app/controllers/02_projects/13_OtherCtrl',
 
-		'directives'     		: '../js/app/directives/directives',
-		'jQuery'		  		: '../libs/js/jquery.min',
-		'jQueryUI'        		: '../libs/js/jquery-ui.min',
-		'angular'         		: '../libs/js/angular',
-		'angularAnimate'  		: '../libs/js/angular-animate.min',
-		'angularToastr'	  		: '../libs/js/angular-toastr.tpls.min',
-		'angularRoute'	  		: '../libs/js/angular_route',
-		'twitterBootstrap' 		: '../libs/js/bootstrap.min',
-		'angularRuLocale'  		: _env()._angularRuLocale // 'https://code.angularjs.org/1.3.5/i18n/angular-locale_ru-by'
-	},
-
-	shim : 
-	{
-		'jQueryUI' : {
-		 	deps : ['jQuery'],
-			exports : 'jQueryUI'
-		},
-		'angular' : {
-			deps : ['jQueryUI'],
-			exports : 'angular'
-		},
-        'angularRoute' : {
-            deps : ['angular'],
-            exports : 'angularRoute'
-        },
-		'angularAnimate' : {
-			deps : ['angularRoute'],
-			exports : 'angularAnimate'
-		},
-		'angularToastr' : {
-			deps : ['angularAnimate'],
-			exports : 'angularToastr'
-		},
-        'usersCtrlModule' : {
-            deps : ['controllers']
-        },
-        'adminCtrlModule' : {
-            deps : ['controllers']
-        },
-        'newProjectCtrlModule' : {
-            deps : ['controllers'],
-            exports : 'newProjectCtrlModule'
-        },
-        'budgetCtrlModule' : {
-            deps : ['controllers']
-        },
-        'restaurantCtrlModule' : {
-            deps : ['controllers']
-        },
-        'arrangementCtrlModule' : {
-            deps : ['controllers']
-        },
-        'filmingCtrlModule' : {
-            deps : ['controllers']
-        },
-        'registrationCtrlModule' : {
-            deps : ['controllers']
-        },
-        'marriageCtrlModule' : {
-            deps : ['controllers']
-        },
-        'transportCtrlModule' : {
-            deps : ['controllers']
-        },
-        'dressCtrlModule' : {
-            deps : ['controllers']
-        },
-        'partyCtrlModule' : {
-        	deps : ['controllers']
-    	},
-        'otherCtrlModule' : {
-            deps : ['controllers']
+            'directives'     		: '../js/app/directives/directives',
+            'jQuery'		  		: '../libs/js/jquery.min',
+            'jQueryUI'        		: '../libs/js/jquery-ui.min',
+            'angular'         		: '../libs/js/angular',
+            'angularAnimate'  		: '../libs/js/angular-animate.min',
+            'angularToastr'	  		: '../libs/js/angular-toastr.tpls.min',
+            'angularRoute'	  		: '../libs/js/angular_route',
+            'twitterBootstrap' 		: '../libs/js/bootstrap.min',
+            'angularRuLocale'  		: _env()._angularRuLocale // 'https://code.angularjs.org/1.3.5/i18n/angular-locale_ru-by'
         },
 
-		'twitterBootstrap' : ['jQuery'],
-		'app' : {
-			deps : [
-				'angularToastr',
-				'angularRoute',
-				'twitterBootstrap',
+    shim :
+        {
+            'jQueryUI' : {
+                deps : ['jQuery'],
+                exports : 'jQueryUI'
+            },
+            'angular' : {
+                deps : ['jQueryUI'],
+                exports : 'angular'
+            },
+            'angularRoute' : {
+                deps : ['angular'],
+                exports : 'angularRoute'
+            },
+            'angularAnimate' : {
+                deps : ['angularRoute'],
+                exports : 'angularAnimate'
+            },
+            'angularToastr' : {
+                deps : ['angularAnimate'],
+                exports : 'angularToastr'
+            },
+            'usersCtrlModule' : {
+                deps : ['controllers']
+            },
+            'adminCtrlModule' : {
+                deps : ['controllers']
+            },
+            'newProjectCtrlModule' : {
+                deps : ['controllers'],
+                exports : 'newProjectCtrlModule'
+            },
+            'budgetCtrlModule' : {
+                deps : ['controllers']
+            },
+            'restaurantCtrlModule' : {
+                deps : ['controllers']
+            },
+            'arrangementCtrlModule' : {
+                deps : ['controllers']
+            },
+            'filmingCtrlModule' : {
+                deps : ['controllers']
+            },
+            'registrationCtrlModule' : {
+                deps : ['controllers']
+            },
+            'marriageCtrlModule' : {
+                deps : ['controllers']
+            },
+            'transportCtrlModule' : {
+                deps : ['controllers']
+            },
+            'dressCtrlModule' : {
+                deps : ['controllers']
+            },
+            'partyCtrlModule' : {
+                deps : ['controllers']
+            },
+            'otherCtrlModule' : {
+                deps : ['controllers']
+            },
 
-				'projServices',
-				'authServices',
+            'twitterBootstrap' : ['jQuery'],
+            'app' : {
+                deps : [
+                    'angularToastr',
+                    'angularRoute',
+                    'twitterBootstrap',
 
-				'usersCtrlModule',
-                'adminCtrlModule',
+                    'projServices',
+                    'authServices',
 
-				'newProjectCtrlModule',
+                    'usersCtrlModule',
+                    'adminCtrlModule',
 
-				'budgetCtrlModule',
-				'restaurantCtrlModule',
-				'arrangementCtrlModule',
-                'programCtrlModule',
-                'filmingCtrlModule',
-                'registrationCtrlModule',
-				'marriageCtrlModule',
-    			'transportCtrlModule',
-				'dressCtrlModule',
-                'partyCtrlModule',
-                'otherCtrlModule',
+                    'newProjectCtrlModule',
 
-				'directives'
-			]
-		}
-	},
+                    'budgetCtrlModule',
+                    'restaurantCtrlModule',
+                    'arrangementCtrlModule',
+                    'programCtrlModule',
+                    'filmingCtrlModule',
+                    'registrationCtrlModule',
+                    'marriageCtrlModule',
+                    'transportCtrlModule',
+                    'dressCtrlModule',
+                    'partyCtrlModule',
+                    'otherCtrlModule',
 
-	deps : ['../js/bootstrap']
+                    'directives'
+                ]
+            }
+        },
+
+    deps : ['../js/bootstrap']
 
 });
