86127a55c9fc758105ac995bb04ad5368ce50c73
Aleksey-Kolyasa-dev <aleksey.kolyasa.dev@gmail.com>
2017-10-10 17:13:18 +0200

    - -save

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,14 @@
 "use strict";
 const gulp = require('gulp');
+const sourcemaps = require('gulp-sourcemaps');
+const debug = require('gulp-debug');
+const gulpIf = require('gulp-if');
+const del = require('del');
 
-gulp.task('hello', function (callback) {
-   console.log('hello');
-   callback();
+const isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV == 'development';
+
+gulp.task('default', function () {
+  return gulp.src('public/**/*.*')
+      .pipe(debug())
+      .pipe(gulp.dest('frontend'));
 });
